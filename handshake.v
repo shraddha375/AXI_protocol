4a30cf005f759b2dac3f4d7a07929a899e8245e3
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-08-07 12:53:03 -0400

    Update handshake.v

--- handshake.v
+++ handshake.v
@@ -14,79 +14,71 @@ module handshake();
     reg clk = 0;
      
     always #10 clk = ~clk;
-     
+
+    // Handling the rstn signal
     initial begin
-    rstn = 0;
-    repeat(10) @(posedge clk);
-    rstn = 1;
+         rstn = 0;
+         repeat(10) @(posedge clk);
+         rstn = 1;
     end
      
     //////////// master logic
      
-    always@(posedge clk)
-    begin
-      if(rstn==1'b0)
-        begin
-        m_data <= 0;
-        m_validout <= 0;
-        end
-      else
-       begin
-        case(m_state)
-        new_data: 
-            begin
-               m_data <= $urandom_range(0,15);
-               m_validout <= 1'b1;
-               m_state      <= wait_for_slave;
-            end
+     always@(posedge clk) begin
+          if(rstn == 1'b0) begin
+               m_data <= 0;
+               m_validout <= 0;
+          end
+          else begin
+               case(m_state)
+                    new_data: 
+                    begin
+                         m_data <= $urandom_range(0,15);
+                         m_validout <= 1'b1;
+                         m_state    <= wait_for_slave;
+                    end
             
-        wait_for_slave: 
-            begin
-               if(s_readyout)
-                 begin
-                 m_state <= new_data;
-                 m_validout <= 1'b0;
-                 end
-               else
-                 m_state <= wait_for_slave;
-            end 
-        endcase 
-       end
-    end
+                    wait_for_slave: 
+                    begin
+                         if(s_readyout) begin
+                              m_state <= new_data;
+                              m_validout <= 1'b0;
+                         end
+                         else
+                              m_state <= wait_for_slave;
+                    end 
+               endcase 
+          end
+     end
      
-    /////////////////////// slave logic
+     /////////////////////// slave logic
      
-    always@(posedge clk)
-    begin
-      if(rstn==1'b0)
-        begin
-        s_data     <= 0;
-        s_readyout <= 0;
-        end
-      else
-       begin
-        case(s_state)
-        wait_for_data: 
-            begin
-                s_readyout <= 1'b1;
-                if(m_validout == 1'b1)
-                   begin
-                   s_state     <= process_data;
-                   s_readyout <= 1'b0;
-                   s_data      <= m_data;
-                   end
-                else
-                   s_state <= wait_for_data; 
-            end
-        process_data: 
-            begin
-                s_state <= wait_for_data;
-                s_readyout <= 1'b1; 
-            end 
-        endcase 
-       end
-    end
-     
-     
-     
-    endmodule
+     always@(posedge clk) begin
+          if(rstn==1'b0) begin
+               s_data     <= 0;
+               s_readyout <= 0;
+          end
+          else begin
+               case(s_state)
+                    wait_for_data: 
+                    begin
+                         s_readyout <= 1'b1;
+                         if(m_validout == 1'b1) begin
+                              s_state     <= process_data;
+                              s_readyout <= 1'b0;
+                              s_data      <= m_data;
+                         end
+                         else
+                              s_state <= wait_for_data; 
+                    end
+                    
+                    process_data: 
+                    begin
+                         s_state <= wait_for_data;
+                         s_readyout <= 1'b1; 
+                    end 
+               endcase 
+          end
+     end
+
+endmodule
