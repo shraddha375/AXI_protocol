21cbbb298d421ce551a3ef399c2e734436ade6e1
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-11-27 15:21:14 +0530

    Update axis_module.sv

--- AXI Stream Interface/AXIS/axis_module.sv
+++ AXI Stream Interface/AXIS/axis_module.sv
@@ -1,7 +1,6 @@
 ///////////////////Design Code: Connecting master and slave together
 
-    module top 
-    (
+module top (
     input clk,rst, newd,
     input [7:0] din,
     output [7:0] dout,
@@ -15,41 +14,4 @@
      
     assign last = last_t;
      
-    endmodule
-
-////////////TB Code
-
-
-    module top_tb();
-     
-     
-    reg clk = 0;
-    reg rst;
-    reg newd;
-    reg [7:0] din;
-    wire [7:0] dout;
-    wire last;
-     
-     
-    top dut(clk,rst, newd, din, dout, last);
-     
-     
-     always #10 clk = ~clk;
-     
-     initial 
-        begin
-            // Initialize inputs
-            rst = 1'b0;
-            repeat(10) @(posedge clk);
-            rst = 1'b1;
-            for(int i = 0; i <10; i++)
-            begin
-            @(posedge clk);
-            newd = 1;
-            din = $urandom_range(0,15);
-            @(negedge last);
-            end
-            $finish;
-        end
-     
-    endmodule
+endmodule
